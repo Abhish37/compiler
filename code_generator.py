3d6d62870e25f79e23059673e177eecc3676addd
Abhish37 <abhishj2004@gmail.com>
2025-04-09 12:27:34 +0530

    Delete code_generator.py

--- code_generator.py
+++
@@ -1,6 +0,0 @@
-def code_generator(parsed_instructions):
-    for instr in parsed_instructions:
-        if instr['type'] == 'ABH':
-            n = instr['value']
-            result = 3 ** n
-            print(f"3^{n} = {result}")
