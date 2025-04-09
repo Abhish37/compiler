e0c05252b99da9572927f1ba9d0070f958cd4792
Abhish37 <abhishj2004@gmail.com>
2025-04-09 12:27:57 +0530

    Delete sementic_analysis.py

--- sementic_analysis.py
+++
@@ -1,5 +0,0 @@
-def semantic_analysis(parsed_instructions):
-    for instr in parsed_instructions:
-        if instr['type'] == 'ABH':
-            if instr['value'] < 0:
-                raise ValueError("Exponent must be non-negative")
